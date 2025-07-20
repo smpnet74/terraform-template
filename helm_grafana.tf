e1acea649f4fa16054e9c40072499bf5d67f663d
Scott Peterson <sm_peterson@yahoo.com>
2025-07-20 13:18:16 -0500

    add default dashboards to grafana

--- helm_grafana.tf
+++ helm_grafana.tf
@@ -70,6 +70,157 @@ resource "helm_release" "grafana" {
     name  = "dashboardProviders.dashboardproviders\\.yaml.providers[0].options.path"
     value = "/var/lib/grafana/dashboards/default"
   }
+
+  # Pre-configured dashboards for our cloud-native stack
+  set {
+    name  = "dashboards.default.kubernetes-cluster-monitoring.gnetId"
+    value = "7249"  # Kubernetes Cluster Monitoring (via Prometheus)
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-cluster-monitoring.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-cluster-monitoring.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.node-exporter.gnetId"
+    value = "1860"  # Node Exporter Full
+  }
+  
+  set {
+    name  = "dashboards.default.node-exporter.revision"
+    value = "37"
+  }
+  
+  set {
+    name  = "dashboards.default.node-exporter.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.kubernetes-pods.gnetId"
+    value = "6417"  # Kubernetes Pods
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-pods.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-pods.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.istio-control-plane.gnetId"
+    value = "7645"  # Istio Control Plane Dashboard
+  }
+  
+  set {
+    name  = "dashboards.default.istio-control-plane.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.istio-control-plane.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.istio-mesh.gnetId"
+    value = "7639"  # Istio Mesh Dashboard
+  }
+  
+  set {
+    name  = "dashboards.default.istio-mesh.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.istio-mesh.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.cilium-agent.gnetId"
+    value = "16611"  # Cilium Metrics
+  }
+  
+  set {
+    name  = "dashboards.default.cilium-agent.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.cilium-agent.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.prometheus-stats.gnetId"
+    value = "2"  # Prometheus 2.0 Stats
+  }
+  
+  set {
+    name  = "dashboards.default.prometheus-stats.revision"
+    value = "2"
+  }
+  
+  set {
+    name  = "dashboards.default.prometheus-stats.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.kubernetes-apiserver.gnetId"
+    value = "12006"  # Kubernetes API Server
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-apiserver.revision"
+    value = "1"
+  }
+  
+  set {
+    name  = "dashboards.default.kubernetes-apiserver.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.kube-state-metrics.gnetId"
+    value = "13332"  # Kube State Metrics v2
+  }
+  
+  set {
+    name  = "dashboards.default.kube-state-metrics.revision"
+    value = "12"
+  }
+  
+  set {
+    name  = "dashboards.default.kube-state-metrics.datasource"
+    value = "Prometheus"
+  }
+
+  set {
+    name  = "dashboards.default.alertmanager.gnetId"
+    value = "9578"  # Alertmanager
+  }
+  
+  set {
+    name  = "dashboards.default.alertmanager.revision"
+    value = "4"
+  }
+  
+  set {
+    name  = "dashboards.default.alertmanager.datasource"
+    value = "Prometheus"
+  }
   
   depends_on = [
     # This will implicitly wait for whichever Prometheus is enabled
